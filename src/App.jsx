99db92c154b536ef3c4e53e620a09afe01d4dc3c
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-07-20 21:38:22 +0200

    good final version

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,62 @@ import './App.css';
 import { Counter } from './components/Counter';
 import { SingleLight } from './components/SingleLight';
 import { Text } from './components/Text';
+import { useState } from 'react';
+import { Grid } from './components/Grid';
 
 function App() {
+  const [rows, setRows] = useState(0);
+  const [columns, setColumns] = useState(0);
+
+  const handleIncrementRow = () => {
+    setRows(rows + 1);
+  };
+  const handleDecrementRow = () => {
+    if (rows <= 0) {
+      return;
+    }
+    setRows(rows - 1);
+  };
+
+  const handleIncrementColumn = () => {
+    if (columns >= 9) {
+      return;
+    }
+    setColumns(columns + 1);
+  };
+  const handleDecrementColumn = () => {
+    if (columns <= 0) {
+      return;
+    }
+    setColumns(columns - 1);
+  };
+
+  const totalLights = rows * columns;
+
+  const lights = Array.from({ length: totalLights });
+
   return (
     <div>
       <nav className='nav'>
-        <Text>0 bulbs</Text>
-        <Counter text={'rows'} count={0} />
-        <Counter text={'columns'} count={0} />
+        <Text>{totalLights} bulbs</Text>
+        <Counter
+          text={'rows'}
+          count={rows}
+          onDecrement={handleDecrementRow}
+          onIncrement={handleIncrementRow}
+        />
+        <Counter
+          text={'columns'}
+          count={columns}
+          onDecrement={handleDecrementColumn}
+          onIncrement={handleIncrementColumn}
+        />
       </nav>
-      <div>
-        <SingleLight />
-      </div>
+      <Grid columns={columns}>
+        {lights.map((value, idx) => {
+          return <SingleLight key={idx} />;
+        })}
+      </Grid>
     </div>
   );
 }
--- src/components/SingleLight.jsx
+++ src/components/SingleLight.jsx
@@ -1,15 +1,34 @@
 import { useState } from 'react';
 import LightTurnedOff from '/images/light-turned-off.svg';
 import LightBlue from '/images/light-blue.svg';
+import LightGreen from '/images/light-green.svg';
+import LightOrange from '/images/light-orange.svg';
+import LightPink from '/images/light-pink.svg';
+import LightRed from '/images/light-red.svg';
+import LightYellow from '/images/light-yellow.svg';
+
+const lights = [
+  LightBlue,
+  LightGreen,
+  LightOrange,
+  LightPink,
+  LightRed,
+  LightYellow,
+];
 
 export function SingleLight() {
   const [isOn, setOn] = useState(true);
 
-  const imageSrc = isOn ? LightBlue : LightTurnedOff;
+  const getRandomIndex = () => {
+    return Math.floor(Math.random() * lights.length);
+  };
+
+  const randomIndex = getRandomIndex();
+
+  const imageSrc = isOn ? lights[randomIndex] : LightTurnedOff;
 
   const handleToggle = () => {
     setOn(!isOn);
   };
-
   return <img onClick={handleToggle} src={imageSrc} alt='Single Light' />;
 }
